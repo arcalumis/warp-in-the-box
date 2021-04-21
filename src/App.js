1d97f0ea8d8cef61bfe41ef2b3d018521d3d6107
Baudrillard <dev@hgip.tv>
2021-04-21 14:58:11 -0400

    git test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 export function App() {
   return (
     <div>
-      <h1>Welcome { new Date().toString() }</h1>
+      <h1>Welcome { new Date().toString() } !</h1>
     </div>
   );
 }
